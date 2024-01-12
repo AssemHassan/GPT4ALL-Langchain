7bd2a08ed7b43a8953d368c1b116c3c78ffec991
assem.hassan@principal.software <assem.hassan@principal.software>
2024-01-12 18:24:03 +0200

    add poetry as Py package manager - fix warrnings

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ Created on Mon Jul 17 12:06:50 2023
 @author: dreji18
 """
 
-from langchain.document_loaders import WebBaseLoader
+from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.document_loaders.csv_loader import CSVLoader
 import streamlit as st
 
-from langchain.vectorstores import Chroma
-from langchain.embeddings import GPT4AllEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import GPT4AllEmbeddings
 
-loader = CSVLoader(file_path=r'D:\Personal Projects\gpt4all\marvel app\Marvel Datastore.csv', source_column="Sentences")
+loader = CSVLoader(file_path=r'./Marvel Datastore.csv', source_column="Sentences")
 
 
 st.title("Marvel bot")
