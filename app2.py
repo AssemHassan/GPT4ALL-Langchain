7bbab5f0a631acd26dd0d9529e27ec880849c50d
assem.hassan@gmail.com <assem.hassan@gmail.com>
2024-02-19 10:11:53 +0200

    models in relative folder

--- app2.py
+++ app2.py
@@ -1,17 +1,22 @@
+import os
 import streamlit as st
 from langchain_community.llms import gpt4all
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 from langchain.output_parsers.json import SimpleJsonOutputParser
 
-#PATH = '/home/assem/AI/gpt4all-models/orca-mini-3b-gguf2-q4_0.gguf' #Example SQL Statement: SELECT COUNT(*) FROM Experiment WHERE ExperimentType_ID IN (1,2,3)
-#PATH = '/home/assem/AI/gpt4all-models/nous-hermes-llama2-13b.Q4_0.gguf' #SQL Statement: SELECT COUNT(*) FROM Experiment GROUP BY ExpermentTypeId;
-#PATH = '/home/assem/AI/gpt4all-models/all-MiniLM-L6-v2-f16.gguf'
-#PATH = '/home/assem/AI/gpt4all-models/mistral-7b-openorca.Q4_0.gguf' #SELECT COUNT(*) as NumberOfExperiments, ExperimentTypeId FROM Experiment GROUP BY ExperimentTypeId;
-PATH = '/home/assem/AI/gpt4all-models/mistral-7b-instruct-v0.1.Q4_0.gguf' #SELECT E.Name, COUNT(*) AS Count FROM ExperimentType ET JOIN Experiment E ON ET.Id = E.ExperimentTypeId GROUP BY E.Name ORDER BY Count DESC;
-#PATH = '/home/assem/AI/gpt4all-models/replit-code-v1_5-3b-q4_0.gguf'#?? provides suggestions of several answers
-#PATH = '/home/assem/AI/gpt4all-models/starcoder-q4_0.gguf'#??
-#PATH = '/home/assem/AI/gpt4all-models/gpt4all-falcon-q4_0.gguf'#SELECT COUNT(DISTINCT ExperimentTypeId) AS TypeCount FROM ExperimentType WHERE TypeCount = 10;
+model_path = os.path.abspath('./local-models')
+
+MODEL = 'orca-mini-3b-gguf2-q4_0.gguf' #Example SQL Statement: SELECT COUNT(*) FROM Experiment WHERE ExperimentType_ID IN (1,2,3)
+#MODEL = 'gpt4all-models/nous-hermes-llama2-13b.Q4_0.gguf' #SQL Statement: SELECT COUNT(*) FROM Experiment GROUP BY ExpermentTypeId;
+#MODEL = 'gpt4all-models/all-MiniLM-L6-v2-f16.gguf'
+#MODEL = 'gpt4all-models/mistral-7b-openorca.Q4_0.gguf' #SELECT COUNT(*) as NumberOfExperiments, ExperimentTypeId FROM Experiment GROUP BY ExperimentTypeId;
+#MODEL = 'gpt4all-models/mistral-7b-instruct-v0.1.Q4_0.gguf' #SELECT E.Name, COUNT(*) AS Count FROM ExperimentType ET JOIN Experiment E ON ET.Id = E.ExperimentTypeId GROUP BY E.Name ORDER BY Count DESC;
+#MODEL = 'gpt4all-models/replit-code-v1_5-3b-q4_0.gguf'#?? provides suggestions of several answers
+#MODEL = 'gpt4all-models/starcoder-q4_0.gguf'#??
+#MODEL = 'gpt4all-models/gpt4all-falcon-q4_0.gguf'#SELECT COUNT(DISTINCT ExperimentTypeId) AS TypeCount FROM ExperimentType WHERE TypeCount = 10;
+
+PATH = os.path.join(model_path, MODEL)
 
 llm = gpt4all.GPT4All(model=PATH)
 prompt =  ChatPromptTemplate.from_template(template="""
